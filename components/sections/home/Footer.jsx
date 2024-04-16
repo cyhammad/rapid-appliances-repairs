fb9bd6e665c62f8e7160cf418aab6983d82172d0
Usama Iftikhar <usamaiftikhar718@gmail.com>
2024-04-16 08:46:08 +0500

    phone number added

--- components/sections/home/Footer.jsx
+++ components/sections/home/Footer.jsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const Footer = () => {
+const BoschFooter = () => {
   return (
-    <div className="flex justify-center bg-[#2D3033]">
-      <footer className="flex w-full max-w-7xl flex-col gap-y-5 px-5 pb-10 text-gray-300">
-        <div className="mt-5 flex pt-10 items-center gap-2">
-          <h1 className="text-[20px] font-bold sm:text-[30px] text-white">
-            <span className="text-[#ED0007]">Rapid</span> Appliances Repair Center
-          </h1>
+    <footer className="flex justify-center bg-[#2D3033] text-white">
+      <div className="mt-5 flex w-full max-w-[78rem] flex-col gap-y-5 px-5 py-10">
+        <div className="flex items-center gap-x-1">
+          <Image src="/logo.svg" width={50} height={50} alt="LOGO" />
+          <div className="flex flex-col justify-between px-2">
+            <span className="text-3xl font-bold uppercase">Rapid</span>
+            <span className="text-xs uppercase text-white">
+              Appliances Repairs
+            </span>
+          </div>
         </div>
-
         <span className="text-sm font-light ">
           An authorized service provider specialized in appliance repair and maintenance services in
-          Dubai and Abu Dhabi of all the home appliances.
+          Dubai and Abu Dhabi of all the bosch home appliances.
         </span>
         <div className="flex flex-col gap-y-5 ">
           <span className="hover:underline">Home</span>
@@ -22,12 +25,13 @@ const Footer = () => {
           <span className="hover:underline">Terms of use</span>
           <span className="hover:underline">Privacy Policy</span>
         </div>
-        <span className="border-t border-gray-700 pt-5 text-center text-xs">
-          © 2024 Rapid Repair Center. All rights reserved.
+        <span className="border-t border-[#c2c4c4] pt-5 text-center text-xs">
+          © 2024 Bosch Repair Center. All rights reserved.
         </span>
-      </footer>
-    </div>
+      </div>
+    </footer>
   );
 };
 
-export default Footer;
+export default BoschFooter;
+
