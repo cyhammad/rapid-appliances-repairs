282556db771510720bf749171e0d300652c69fcd
Usama Iftikhar <usamaiftikhar718@gmail.com>
2024-04-25 17:08:17 +0500

    gtag added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={inter.className}>
+    <head>
+        {/* Include gtag.js script */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-16532858519" />
+
+        {/* Include gtag.js initialization script */}
+        <Script dangerouslySetInnerHTML={{ __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-16532858519');
+        ` }} />
+
+      
       {children}
+      </head>
     </html>
   );
 }
